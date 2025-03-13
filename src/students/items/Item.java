21dfc33bb21d9c1bc6d9892d8fb62030a3fa1015
Leif McLean <mclli001@mymail.unisa.edu.au>
2025-03-13 14:13:19 +1030

    Added Item Abstract Class
    Added initial implementation of the item abstract class. Added the required methods excluding the equals method.

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,48 @@
 package students.items;
 
-public class Item {
+public abstract class Item {
+	int age = 0;
+	int maturationAge;
+	int deathAge;
+	double value;
+	
+	public Item(int maturationAge, int deathAge, double value) {
+		this.maturationAge = maturationAge; 
+		this.deathAge = deathAge;
+		this.value = value;
+		
+	}
+	
+	public void tick() {
+		this.age++;
+	}
+	
+	public void setAge(int newAge) {
+		this.age = newAge;
+	}
+	
+	public boolean died(){
+		if (this.age >= this.deathAge)
+			return true;
+		else 
+			return false;
+	}
+	
+	public double getValue() {
+		if (this.age >= this.maturationAge) {
+			return this.value;
 
+		}
+		else 
+			return 0;
+	}
+	
+	// TO DO: equals 
+	
+	@Override
+	public abstract String toString();
+		
+		
+	
 	
 }
